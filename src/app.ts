3dfd83ca5d507f292f0f54109f8f7df27209eeb6
jatinkumar-me <jatin208015@gmail.com>
2024-01-25 23:23:27 +0530

    adding websocket

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,22 @@ import Tools from "./tools/tools";
 
 export default class App {
     baseLayer: BaseLayer;
+    connection: WebSocket;
     tool: Tools;
     
-    constructor() {
+    constructor(connectionUrl: string) {
+        this.connection = new WebSocket(connectionUrl);
         this.baseLayer = new BaseLayer();
         this.tool = new Pencil(this.baseLayer);
     }
 
     init() {
         console.log("app started");
+    }
+
+    onClose() {
+        this.connection.onmessage = (ev: MessageEvent<string>) => {
+            ev.data
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import App from './app';
 import './style.css'
 
 function main() {
-    const app = new App();
+    const app = new App('ws://127.0.0.1:8080/');
     app.init();
 }
 
