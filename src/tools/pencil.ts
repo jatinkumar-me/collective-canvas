76ebbf0cf444c5bcaa3a9c77834484a756869b9e
jatinkumar-me <jatin208015@gmail.com>
2024-02-14 23:13:13 +0530

    formatting fix

--- src/tools/pencil.ts
+++ src/tools/pencil.ts
@@ -1,7 +1,7 @@
 import BaseLayer from "../components/Layer";
-import Tools from "./tools";
+import BaseTools from "./tools";
 
-export default class Pencil extends Tools {
+export default class Pencil extends BaseTools {
   ctx: CanvasRenderingContext2D;
   strokeStyle: string | CanvasGradient | CanvasPattern;
   lineCap: CanvasLineCap;
@@ -18,16 +18,16 @@ export default class Pencil extends Tools {
     this.speedDependenceFactor = 0;
     this.events();
     this.ctx = baseLayer.ctx;
-    this.mouseSpeedCoefficient = 2
+    this.mouseSpeedCoefficient = 2;
   }
 
   events() {
-    document.addEventListener('mousedown', this.onMouseDown.bind(this))
-    document.addEventListener('mouseup', this.onMouseUp.bind(this))
-    document.addEventListener('mousemove', this.onMouseMove.bind(this))
-    document.addEventListener('wheel', this.onWheel.bind(this), {
-      passive: false
-    })
+    document.addEventListener("mousedown", this.onMouseDown.bind(this));
+    document.addEventListener("mouseup", this.onMouseUp.bind(this));
+    document.addEventListener("mousemove", this.onMouseMove.bind(this));
+    document.addEventListener("wheel", this.onWheel.bind(this), {
+      passive: false,
+    });
   }
 
   onMouseMove(event: MouseEvent): void {
@@ -51,7 +51,7 @@ export default class Pencil extends Tools {
   }
 
   onWheel(event: WheelEvent): void {
-    if ((event.target as HTMLElement).id === 'canvas') {
+    if ((event.target as HTMLElement).id === "canvas") {
       event.preventDefault();
     }
     this.mouseSpeedCoefficient += event.deltaY * 0.01;
@@ -64,11 +64,16 @@ export default class Pencil extends Tools {
       return this.strokeWidth;
     }
 
-    const temp = (this.speedDependenceFactor > 0 ?
-      this.mouseAverageSpeed * this.speedDependenceFactor :
-      this.MAX_STROKE_WIDTH + this.mouseAverageSpeed * this.speedDependenceFactor);
+    const temp =
+      this.speedDependenceFactor > 0
+        ? this.mouseAverageSpeed * this.speedDependenceFactor
+        : this.MAX_STROKE_WIDTH +
+          this.mouseAverageSpeed * this.speedDependenceFactor;
 
-    this.strokeWidth = Math.min(this.MAX_STROKE_WIDTH * this.mouseSpeedCoefficient, temp);
+    this.strokeWidth = Math.min(
+      this.MAX_STROKE_WIDTH * this.mouseSpeedCoefficient,
+      temp
+    );
     return this.strokeWidth;
   }
 
@@ -82,18 +87,4 @@ export default class Pencil extends Tools {
     this.ctx.beginPath();
     this.ctx.moveTo(this.mouseLastPosition[0], this.mouseLastPosition[1]);
   }
-
-  // draw_v2() {
-  //   var dist_x = from_x - to_x;
-  //   var dist_y = from_y - to_y;
-  //   var distance = Math.sqrt((dist_x * dist_x) + (dist_y * dist_y));
-  //   var radiance = Math.atan2(dist_y, dist_x);
-  //
-  //   for (var j = 0; j < distance; j++) {
-  //     var x_tmp = Math.round(to_x + Math.cos(radiance) * j) - Math.floor(size / 2) - 1;
-  //     var y_tmp = Math.round(to_y + Math.sin(radiance) * j) - Math.floor(size / 2) - 1;
-  //
-  //     ctx.fillRect(x_tmp, y_tmp, size, size);
-  //   }
-  // }
 }
