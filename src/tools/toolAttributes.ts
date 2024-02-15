802e9ef4df0409851636cb9cb673adda0406add6
jatinkumar-me <jatin208015@gmail.com>
2024-02-15 23:48:53 +0530

    new method
    method to add input markup for tool attributes

--- src/tools/toolAttributes.ts
+++ src/tools/toolAttributes.ts
@@ -13,14 +13,32 @@ export type ToolAttributesMarkup<T> = Partial<
 
 export default class ToolAttributes {
   toolBoxDiv: HTMLDivElement | null;
-  constructor() {
+  toolAttributesMarkup: Record<string, string>
+  constructor(toolAttributesMarkup: Record<string, string>) {
+    this.toolAttributesMarkup = toolAttributesMarkup;
     this.toolBoxDiv = document.getElementById(
       "tool-attributes"
     ) as HTMLDivElement | null;
     if (this.toolBoxDiv === null) {
       console.error("toolbox div not present in dom");
     }
+    this.insertToolAttributeMarkup();
   }
 
-  events() {}
+  insertToolAttributeMarkup() {
+    if (!this.toolBoxDiv) {
+      return;
+    }
+
+    const toolBoxInnerHTML = Object.values(this.toolAttributesMarkup).join('');
+    this.toolBoxDiv.innerHTML = toolBoxInnerHTML;
+  }
+
+  destroy() {
+    if (!this.toolBoxDiv) {
+      return;
+    }
+
+    this.toolBoxDiv.innerHTML = ''
+  }
 }
