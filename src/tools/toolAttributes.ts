e30f97f7206b5a7d1c355457f8256174e1b551bd
jatinkumar-me <jatin208015@gmail.com>
2024-02-15 15:11:19 +0530

    exclude methods in type

--- src/tools/toolAttributes.ts
+++ src/tools/toolAttributes.ts
@@ -1,14 +1,26 @@
-export type DefaultToolAttributes<T> = Omit<T, keyof ToolAttributes>;
+export type DefaultToolAttributes<T> = Omit<
+  ExcludeMethods<T>,
+  keyof ToolAttributes
+>;
+
+type ExcludeMethods<T> = {
+  [K in keyof T as T[K] extends Function ? never : K]: T[K];
+};
+
+export type ToolAttributesMarkup<T> = Partial<
+  Record<keyof DefaultToolAttributes<T>, string>
+>;
 
 export default class ToolAttributes {
   toolBoxDiv: HTMLDivElement | null;
   constructor() {
-    this.toolBoxDiv = document.getElementById('toolbox') as HTMLDivElement | null;
+    this.toolBoxDiv = document.getElementById(
+      "tool-attributes"
+    ) as HTMLDivElement | null;
     if (this.toolBoxDiv === null) {
-      console.error("toolbox div not present in dom")
+      console.error("toolbox div not present in dom");
     }
   }
 
-  events() {
-  }
+  events() {}
 }
