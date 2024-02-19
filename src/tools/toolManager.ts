a4679f5193a7eee1ac1d88c117017920981915a3
jatinkumar-me <jatin208015@gmail.com>
2024-02-19 15:48:14 +0530

    bug fix: fixed tool manager attribute bug

--- src/tools/toolManager.ts
+++ src/tools/toolManager.ts
@@ -1,16 +1,29 @@
 import BaseLayer from "../components/Layer";
+import Bezier from "./bezier";
 import Pencil from "./pencil";
 import BaseTools from "./tools";
+
+enum ToolIds {
+  PENCIL = 'pencil',
+  BEZIER = 'bezier',
+}
 
 /**
  * Class to manage the selected tool in the app.
  */
 export default class ToolManager {
   private selectedTool: BaseTools;
+  selectedToolName: ToolIds;
 
   constructor(private baseLayer: BaseLayer) {
     this.selectedTool = new Pencil(baseLayer);
+    this.selectedToolName = ToolIds.PENCIL;
     this.events();
+  }
+
+  public init() {
+    const selectedToolButton = document.getElementById(this.selectedToolName) as HTMLButtonElement;
+    selectedToolButton.setAttribute('disabled', 'true');
   }
 
   events() {
@@ -33,6 +46,10 @@ export default class ToolManager {
       console.warn('toolName not defined as dataset in markup');
       return;
     }
+
+    if (this.selectedToolName === toolName) {
+      return;
+    }
     
     const tool = this.createToolFromName(toolName);
     if (!tool) {
@@ -40,19 +57,26 @@ export default class ToolManager {
       return;
     }
 
-    this.selectTool(tool);
+    this.selectTool(tool, toolName as ToolIds);
+    target.setAttribute('disabled', 'true');
   }
 
   createToolFromName(toolName: string): BaseTools | null {
     switch (toolName) {
-      case 'pencil':
+      case ToolIds.PENCIL:
         return new Pencil(this.baseLayer);
+      case ToolIds.BEZIER:
+        return new Bezier(this.baseLayer);
       default:
         return null;
     }
   }
 
-  selectTool(tool: BaseTools) {
+  selectTool(tool: BaseTools, toolName: ToolIds) {
+    this.selectedTool.destroy();
+    const selectedToolButton = document.getElementById(this.selectedToolName) as HTMLButtonElement;
+    selectedToolButton.disabled = false;
     this.selectedTool = tool;
+    this.selectedToolName = toolName;
   }
 }
