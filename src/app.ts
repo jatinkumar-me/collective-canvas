2cffbf8d4452fa48f84f896dbe708c8bcea26e31
jatinkumar-me <jatin208015@gmail.com>
2024-02-20 17:58:43 +0530

    websocket integration in app

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,20 @@
 import BaseLayer from "./components/Layer";
+import { Connection } from "./modules/network";
 import ToolManager from "./tools/toolManager";
 
 export default class App {
     baseLayer: BaseLayer;
     toolManager: ToolManager;
-    connection: WebSocket;
+    connection: Connection;
     
     constructor(connectionUrl: string) {
-        this.connection = new WebSocket(connectionUrl);
+        this.connection = new Connection(connectionUrl);
         this.baseLayer = new BaseLayer();
-        this.toolManager = new ToolManager(this.baseLayer);
+        this.toolManager = new ToolManager(this.baseLayer, this.connection);
     }
 
     init() {
         console.log("app started");
         this.toolManager.init();
-    }
-
-    onClose() {
-        this.connection.onmessage = (ev: MessageEvent<string>) => {
-            ev.data
-        }
     }
 }
