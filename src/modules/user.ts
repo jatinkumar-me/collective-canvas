bd829b094f084de4620e8287f02ca485c3f82339
jatinkumar-me <jatin208015@gmail.com>
2024-02-24 01:07:11 +0530

    external user class

--- src/modules/user.ts
+++ src/modules/user.ts
@@ -1,3 +1,6 @@
+import { DefaultToolAttributes } from "../tools/toolAttributes";
+import { ToolName } from "../tools/toolManager";
+
 export type UserId = string;
 
 export default class User {
@@ -16,5 +19,23 @@ export default class User {
         userDiv.classList.add('user-div');
         userDiv.innerHTML = `<strong>${this.userName}</strong>`;
         return userDiv;
+    }
+}
+
+export class ExternalUser extends User {
+    x: number;
+    y: number;
+    isDrag: boolean;
+    toolName: ToolName
+    toolAttributes: DefaultToolAttributes<any>
+
+    constructor(userId: string, userName: string) {
+        super(userId, userName);
+        this.x = 0;
+        this.y = 0;
+        this.isDrag = false;
+        this.toolName = ToolName.PENCIL;
+        this.toolAttributes = {};
+        super(userId, userName);
     }
 }
