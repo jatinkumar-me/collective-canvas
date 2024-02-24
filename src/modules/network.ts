259d45aef34bb49da198b3e964712847f42b05cb
jatinkumar-me <jatin208015@gmail.com>
2024-02-24 23:38:58 +0530

    websocket changes

--- src/modules/network.ts
+++ src/modules/network.ts
@@ -1,38 +1,46 @@
-import Pencil from "../tools/pencil";
 import ToolAttributes, { DefaultToolAttributes } from "../tools/toolAttributes";
 import { ToolName } from "../tools/toolManager";
-import User, { UserId } from "./user";
+import User, { ExternalUser, UserId } from "./user";
 import UserManager from "./userManager";
 
 enum SocketMessageKind {
     USER_CONNECTED = 1,
     USER_DISCONNECTED = 2,
-    USER_COMMAND = 3
+    USER_COMMAND = 3,
+    USER_CONNECTION_ACKNOWLEDGED = 4,
 }
 
 export type UserCommand<T extends ToolAttributes> = {
     x: number;
     y: number;
     isDrag: boolean;
-    toolName: ToolName
-    toolAttributes: DefaultToolAttributes<T>
-}
+    toolName: ToolName;
+    toolAttributes: DefaultToolAttributes<T>;
+};
 
-export type SocketMessage = {
-    type: SocketMessageKind.USER_CONNECTED;
-    user: User;
-} | {
-    type: SocketMessageKind.USER_DISCONNECTED;
-    userId: UserId;
-} | {
-    type: SocketMessageKind.USER_COMMAND;
-    command: UserCommand<any>
-}
-
+export type SocketMessage =
+    | {
+        type: SocketMessageKind.USER_CONNECTED;
+        user: ExternalUser;
+    }
+    | {
+        type: SocketMessageKind.USER_DISCONNECTED;
+        userId: UserId;
+    }
+    | {
+        type: SocketMessageKind.USER_COMMAND;
+        userId: UserId;
+        command: UserCommand<any>;
+    }
+    | {
+        type: SocketMessageKind.USER_CONNECTION_ACKNOWLEDGED;
+        user: User;
+        users: ExternalUser[];
+    };
 
 export class Connection {
-    webSocket: WebSocket;
-    userManager: UserManager;
+    private webSocket: WebSocket;
+    private userManager: UserManager;
 
     constructor(connectionURL: string, userManaager: UserManager) {
         this.webSocket = new WebSocket(connectionURL);
@@ -42,11 +50,15 @@ export class Connection {
         this.webSocket.onopen = this.connectionOpenHandler.bind(this);
     }
 
+    private isConnected(): boolean {
+        return this.webSocket.readyState === WebSocket.OPEN;
+    }
+
     connectionOpenHandler() {
         const message: SocketMessage = {
             type: SocketMessageKind.USER_CONNECTED,
-            user: this.userManager.currentUser,
-        }
+            user: this.userManager.getCurrentUser() as ExternalUser,
+        };
         const messageString = JSON.stringify(message);
         this.webSocket.send(messageString);
     }
@@ -54,15 +66,15 @@ export class Connection {
     connectionCloseHandler() {
         const message: SocketMessage = {
             type: SocketMessageKind.USER_DISCONNECTED,
-            userId: this.userManager.currentUser.userId,
-        }
+            userId: this.userManager.getCurrentUser().userId,
+        };
         const messageString = JSON.stringify(message);
         this.webSocket.send(messageString);
     }
 
     socketMessageHandler(event: MessageEvent<string>) {
         const data = JSON.parse(event.data) as SocketMessage;
-        console.log(data);
+        console.log("Incoming message", data);
         switch (data.type) {
             case SocketMessageKind.USER_CONNECTED:
                 this.handleUserConnected(data.user);
@@ -71,7 +83,10 @@ export class Connection {
                 this.handleUserDisconnected(data.userId);
                 break;
             case SocketMessageKind.USER_COMMAND:
-                this.handleUserCommand(data.command);
+                this.userManager.handleUserCommand(data.userId, data.command);
+                break;
+            case SocketMessageKind.USER_CONNECTION_ACKNOWLEDGED:
+                this.handlUserConnectionAcknowledged(data.user, data.users);
                 break;
             default: {
                 console.error("invalid message sent by the socket");
@@ -79,29 +94,28 @@ export class Connection {
         }
     }
 
-    handleUserConnected(user: User) {
-        this.userManager.addUser(user)
+    handlUserConnectionAcknowledged(user: User, existingUsers: ExternalUser[]) {
+        this.userManager.setCurrentUser(user.userId, user.userName);
+        this.userManager.addMany(existingUsers);
+    }
+
+    handleUserConnected(user: ExternalUser) {
+        this.userManager.addUser(user);
     }
 
     handleUserDisconnected(userId: UserId) {
         this.userManager.removeUser(userId);
     }
 
-    handleUserCommand<T extends ToolAttributes>(command: UserCommand<T>) {
-        switch(command.toolName) {
-            case ToolName.PENCIL:
-                Pencil.drawSegment
-                break;
-            case ToolName.BEZIER:
-                break;
-        }
-    }
-
     sendUserCommand<T extends ToolAttributes>(command: UserCommand<T>) {
+        if (!this.isConnected()) {
+            return;
+        }
         const message: SocketMessage = {
             type: SocketMessageKind.USER_COMMAND,
-            command: command
-        }
+            userId: this.userManager.getCurrentUser().userId,
+            command: command,
+        };
         const messageString = JSON.stringify(message);
         this.webSocket.send(messageString);
     }
