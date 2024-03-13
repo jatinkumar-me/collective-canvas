26239316c7c9418eb6e56b8ac68759edd76d03a9
jatinkumar-me <jatin208015@gmail.com>
2024-03-13 23:47:25 +0530

    refactor: change how markup is generated

--- src/tools/toolAttributes.ts
+++ src/tools/toolAttributes.ts
@@ -40,10 +40,10 @@ export type ToolAttributeInputParam<T extends ToolAttributes> = Partial<
 
 export default abstract class ToolAttributes {
   toolBoxDiv: HTMLDivElement | null;
-  toolAttributesMarkup: Record<string, string>
+  toolAttributesMarkup: string;
 
-  constructor(toolAttributesMarkup: Record<string, string>) {
-    this.toolAttributesMarkup = toolAttributesMarkup;
+  constructor(toolAttributeInput: ToolAttributeInputParam<any>) {
+    this.toolAttributesMarkup = this.generateToolMarkup(toolAttributeInput);
     this.toolBoxDiv = document.getElementById(
       "tool-attributes"
     ) as HTMLDivElement | null;
@@ -53,13 +53,53 @@ export default abstract class ToolAttributes {
     this.insertToolAttributeMarkup();
   }
 
+  private generateToolMarkup(toolAttributeInput: ToolAttributeInputParam<any>): string {
+    let inputMarkup = '';
+    for (let key in toolAttributeInput) {
+      const field = toolAttributeInput[key]
+      if (!field) {
+        continue;
+      }
+
+      let fieldMarkup = '';
+      switch (field.type) {
+        case 'color':
+          fieldMarkup = `<div><label for="${key}">${field.label}</label><input type="color" id="${key}" /></div>`;
+          break;
+        case 'range':
+          fieldMarkup = `<div>
+                          <label for="${key}">${field.label}</label>
+                          <input type="range" id="${key}" name="${key}" min="${field.min}" max="${field.max}" step="${field.step ?? 1}" value="${field.default}">
+                        </div>`;
+          break;
+        case 'select':
+          fieldMarkup = `<div>
+                          <label for="${key}">${field.label}</label>
+                          <select name="${key}" id="${key}" >
+                            ${field.options.map((option) => `<option value=${option} ${field.default === option ? "selected" : ""} >${option}</option>`).join('')}
+                          </select>
+                        </div>`;
+          break;
+        case 'checkbox':
+          fieldMarkup = `<div>
+                          <input type="checkbox" id="${key}" />
+                          <label for="${key}" title="${field.tooltip}">${field.label}</label>
+                        </div>`;
+          break;
+      }
+
+      inputMarkup += fieldMarkup;
+    }
+
+    return inputMarkup;
+  }
+
   insertToolAttributeMarkup() {
     if (!this.toolBoxDiv) {
       return;
     }
 
-    const toolBoxInnerHTML = Object.values(this.toolAttributesMarkup).join('');
-    this.toolBoxDiv.innerHTML = `<fieldset><legend>Tool Attributes</legend>${toolBoxInnerHTML}</fieldset>`;
+    this.toolBoxDiv.innerHTML = `<fieldset><legend>Tool Attributes</legend>${this.toolAttributesMarkup}</fieldset>`;
   }
 
   destroy() {
